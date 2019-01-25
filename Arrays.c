f08af136ed9ddf1fa6ec7621b6367c02146d946d
monycky <monyckyfrazao@gmail.com>
2019-01-25 10:26:49 -0300

    1d array

---
+++ Arrays.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NUMBER_OF_CLASSROOMS 20
+
+
+//Functions protopytes
+void printArray( char dataName[], int dataSet[], int dataLength);
+
+int main(void){
+
+    int classrooms[NUMBER_OF_CLASSROOMS] = {15, 18, 10, 23, 15};
+
+    classrooms[3] =+ 2;
+    printf("The 5th element is %d \n\n", classrooms[4]);
+
+    printArray("classrooms", classrooms, NUMBER_OF_CLASSROOMS);
+
+    return EXIT_SUCCESS;
+
+}
+
+void printArray(char dataName[], int dataSet[], int dataLength){
+    for(int i =0; i<NUMBER_OF_CLASSROOMS; i++){
+        printf("%s[%d]: %d\n", dataName, i, dataSet[i]);
+    }
+}
