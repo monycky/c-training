97edf169a794017cf931219e4f102449b259da60
monycky <monyckyfrazao@gmail.com>
2019-01-28 15:00:18 -0300

    Number Factorial

---
+++ NumberFactorial.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+unsigned int factorial(unsigned int i){
+    
+    if(i <= 1){
+    return 1;
+    }
+
+    return i * factorial(i -1);
+}
+
+int main(){
+
+int i = 32;
+
+printf("Factorial of %d is %d\n", i, factorial(i));
+
+return 0;
+
+}
