a139de4ca724e03156a988ca913f43588b8ab91f
monycky <monyckyfrazao@gmail.com>
2019-01-28 15:05:18 -0300

    Preprocessors

---
+++ Preprocessors.c
@@ -0,0 +1,14 @@
+// #define MAX_ARRAY_LENGHT 20;
+
+#include <stdio.h>
+//#include <"myheader.h">
+
+int main(){
+
+    printf("File: %s\n", __FILE__);
+    printf("Date: %s\n", __DATE__); 
+    printf("Time: %s\n", __TIME__);
+    printf("Line: %d\n", __LINE__);
+    printf("ANSI: %d\n", __STDC__);
+
+}
