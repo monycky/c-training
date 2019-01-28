8bfd3d3132d5b25adae568efa79cc16dd87cf75a
monycky <monyckyfrazao@gmail.com>
2019-01-28 09:39:20 -0300

    Decrementing Pointer

---
+++ DecrementingPointer.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+const int MAX = 3;
+
+int main(){
+    int var[] = {10, 100, 200};
+    int i, *ptr;
+
+    ptr = &var[MAX -1];
+
+    for(i = MAX; i > 0; i--){
+        
+        printf("Address of var var[%d] = %p\n", i-1, ptr);
+        printf("Value of var [%d] = %d\n", i-1, *ptr);
+
+       ptr--;
+    }
+    return 0;
+}
